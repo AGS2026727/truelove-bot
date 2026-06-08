c7112d24407246f3eeca0dc1f176328ef9fc515f
Toth Adam <ags.marine@outlook.com>
2026-06-07 22:01:07 -0300

    Update23 bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -21,7 +21,7 @@ GROQ_API_KEY    = os.environ.get("GROQ_API_KEY")
 WEBHOOK_URL     = os.environ.get("WEBHOOK_URL", "https://truelove-webhook.onrender.com")
 
 # Modelo estável com alta cota diária e por minuto (TPM)
-GROQ_MODEL      = "mixtral-8x7b-32768"
+GROQ_MODEL      = "llama-3.3-70b-versatile"
 
 groq_client = Groq(api_key=GROQ_API_KEY)
 
